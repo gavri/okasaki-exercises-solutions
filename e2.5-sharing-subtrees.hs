ea0fc72b360b3b3f18ec3eb3029558fc09073962
Gavri Fernandez <gavri.fernandez@gmail.com>
2013-09-08 01:22:50 +0530

    e2.5b: incomplete but as balanced as possible binary trees with sharing

--- e2.5-sharing-subtrees.hs
+++ e2.5-sharing-subtrees.hs
@@ -6,3 +6,14 @@ complete x 0 = Empty
 
 complete x d = Bin x child child
         where child = complete x (d - 1)
+
+split n = (ceiling half, floor half)
+        where half = (fromIntegral n) / 2
+
+incomplete x 1 = Bin x Empty Empty
+incomplete x 0 = Empty
+incomplete x n | even numberOfChildrenLeft = Bin x evenChild evenChild
+               | otherwise = Bin x (incomplete x first) (incomplete x second)
+                    where numberOfChildrenLeft = n - 1
+                          (first, second) = split numberOfChildrenLeft
+                          evenChild = (incomplete x (div numberOfChildrenLeft 2))
