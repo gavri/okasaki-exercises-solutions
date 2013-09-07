7383d0b56609ab6fb385d81d751a724921ba4755
Gavri Fernandez <gavri.fernandez@gmail.com>
2013-09-07 22:29:37 +0530

    Zero argument constructors aren't functions
    There is no need for 'empty'

--- e2.5-sharing-subtrees.hs
+++ e2.5-sharing-subtrees.hs
@@ -2,8 +2,7 @@ data Tree a = Empty | Bin a (Tree a) (Tree a) deriving Show
 completeWithNoSharing x 0 = Empty
 completeWithNoSharing x d = Bin x (completeWithNoSharing x (d - 1)) (completeWithNoSharing x (d - 1))
 
-complete x 0 = empty
-        where empty = Empty
+complete x 0 = Empty
 
 complete x d = Bin x child child
         where child = complete x (d - 1)
