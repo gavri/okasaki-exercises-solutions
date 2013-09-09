2c9b2de7e67ae0c4967940568881504f164bcb24
Gavri Fernandez <gavri.fernandez@gmail.com>
2013-09-09 23:11:31 +0530

    e2.5b: Fixed to share all the subtrees that can be shared

--- e2.5-sharing-subtrees.hs
+++ e2.5-sharing-subtrees.hs
@@ -2,18 +2,31 @@ data Tree a = Empty | Bin a (Tree a) (Tree a) deriving Show
 completeWithNoSharing x 0 = Empty
 completeWithNoSharing x d = Bin x (completeWithNoSharing x (d - 1)) (completeWithNoSharing x (d - 1))
 
-complete x 0 = Empty
+complete 0 x = Empty
 
-complete x d = Bin x child child
-        where child = complete x (d - 1)
+complete d x = Bin x child child
+        where child = complete (d - 1) x
 
 split n = (ceiling half, floor half)
         where half = (fromIntegral n) / 2
 
-incomplete x 1 = Bin x Empty Empty
-incomplete x 0 = Empty
-incomplete x n | even numberOfChildrenLeft = Bin x evenChild evenChild
-               | otherwise = Bin x (incomplete x first) (incomplete x second)
+incomplete 1 x = Bin x Empty Empty
+incomplete 0 x = Empty
+incomplete n x | even numberOfChildrenLeft = Bin x evenChild evenChild
+               | otherwise = Bin x (incomplete first x) (incomplete second x)
                     where numberOfChildrenLeft = n - 1
                           (first, second) = split numberOfChildrenLeft
-                          evenChild = (incomplete x (div numberOfChildrenLeft 2))
+                          evenChild = (incomplete (div numberOfChildrenLeft 2) x)
+
+tree x 0 = Empty
+tree x 1 = Bin x Empty Empty
+tree x 2 = Bin x (Bin x Empty Empty) Empty
+tree x 3 = Bin x (Bin x Empty Empty) (Bin x Empty Empty)
+tree x n = Bin x ((trees x) !! n1) ((trees x) !! n2)
+           where (n1, n2) = split (n - 1)
+
+trees x = map (tree x) [0..]
+
+main = do
+        print $ tree 5 3
+        print $ (trees 5) !! 5
