8621c77a070d434f0c96de0cbf93c89ebbceba5c
Gavri Fernandez <gavri.fernandez@gmail.com>
2013-09-10 19:03:44 +0530

    e2.6: Sorted map

---
+++ e2.6-map.hs
@@ -0,0 +1,15 @@
+(|>) a f = f a
+
+data FiniteMap k v = FMEmpty | FMNode k v (FiniteMap k v) (FiniteMap k v) deriving Show
+
+fmSet key value FMEmpty = FMNode key value FMEmpty FMEmpty
+fmSet newKey newValue (FMNode key value left right) | (newKey < key) = (FMNode key value (fmSet newKey newValue left) right)
+                                                   | (newKey > key) = (FMNode key value left (fmSet newKey newValue right))
+                                                   | otherwise = (FMNode key newValue left right)
+
+fmGet lookupKey FMEmpty = Nothing
+fmGet lookupKey (FMNode key value left right) | (lookupKey < key) = fmGet lookupKey left
+                                              | (lookupKey > key) = fmGet lookupKey right
+                                              | otherwise = Just value
+
+main = print $ FMEmpty |> (fmSet 5 "Five") |> (fmSet 6 "Six")
