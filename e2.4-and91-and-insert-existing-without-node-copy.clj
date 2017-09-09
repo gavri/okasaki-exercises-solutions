5a3ede60e8091355056f1e03dedf8718edc1252b
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-09-09 21:06:05 +0700

    Throwing the right exception instead of the exception that was being thrown because I was trying to throw a string

--- e2.4-and91-and-insert-existing-without-node-copy.clj
+++ e2.4-and91-and-insert-existing-without-node-copy.clj
@@ -1,5 +1,5 @@
 (defmulti insert' (fn [e list p] (empty? list)))
-(defmethod insert' true [e list p] (if (= p e) (throw "Key already in tree") [e [] []]))
+(defmethod insert' true [e list p] (if (= p e) (throw (new Exception "Key already in tree")) [e [] []]))
 (defmethod insert' false [e [x l r :as all] p] (cond (< e x) [x (insert' e l p) r]
                                           :else [x l (insert' e r x)]))
 (defn insert [e l] (try (insert' e l (first l)) (catch Exception e l)))
