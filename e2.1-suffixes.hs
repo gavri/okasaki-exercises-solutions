3a147c81c4383c269ef71fbe3512010878eba547
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-09-09 20:44:28 +0700

    Test 2.1

--- e2.1-suffixes.hs
+++ e2.1-suffixes.hs
@@ -1,3 +1,9 @@
+import Test.HUnit
+
 suffixes [] = [[]]
 suffixes xss@(_:xs) = xss : suffixes xs
-main = print $ suffixes [1, 2, 3, 4]
+
+main = runTestTT $ test $ [
+  "Nonterminal" ~: [[1, 2, 3], [2, 3], [3], []] ~=? (suffixes [1, 2, 3]),
+  "Terminal" ~: ([[]] :: [[Int]]) ~=? (suffixes [])
+  ]
