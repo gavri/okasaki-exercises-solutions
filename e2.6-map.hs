50a302b4ba563067e569b4feb7d5d2bfa9b9c1d4
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-09-09 21:40:39 +0700

    Test 2.6

--- e2.6-map.hs
+++ e2.6-map.hs
@@ -1,10 +1,12 @@
+import Test.HUnit
+
 (|>) a f = f a
 
-data FiniteMap k v = FMEmpty | FMNode k v (FiniteMap k v) (FiniteMap k v) deriving Show
+data FiniteMap k v = FMEmpty | FMNode k v (FiniteMap k v) (FiniteMap k v) deriving (Show, Eq)
 
 fmSet key value FMEmpty = FMNode key value FMEmpty FMEmpty
 fmSet newKey newValue (FMNode key value left right) | (newKey < key) = (FMNode key value (fmSet newKey newValue left) right)
-                                                   | (newKey > key) = (FMNode key value left (fmSet newKey newValue right))
+                                                    | (newKey > key) = (FMNode key value left (fmSet newKey newValue right))
                                                    | otherwise = (FMNode key newValue left right)
 
 fmGet lookupKey FMEmpty = Nothing
@@ -12,4 +14,6 @@ fmGet lookupKey (FMNode key value left right) | (lookupKey < key) = fmGet lookup
                                               | (lookupKey > key) = fmGet lookupKey right
                                               | otherwise = Just value
 
-main = print $ FMEmpty |> (fmSet 5 "Five") |> (fmSet 6 "Six")
+main = runTestTT $ test $ [
+  "test" ~: FMNode 5 "Five" FMEmpty (FMNode 6 "Six" FMEmpty FMEmpty) ~=? FMEmpty |> (fmSet 5 "Five") |> (fmSet 6 "Six")
+  ]
