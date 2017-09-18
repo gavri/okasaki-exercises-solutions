ad62a6b0df6e084c76cab909beed0ec7540b2ce2
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-09-18 16:25:30 +0700

    3.2

--- 3-some-familiar-data-structures-in-a-functional-setting/3.2.hs
+++ 3-some-familiar-data-structures-in-a-functional-setting/3.2.hs
@@ -5,7 +5,7 @@ data Heap a = Empty | Bin Int a (Heap a) (Heap a)
 rank Empty = 0
 rank (Bin r e left right) = r
 
-makeBin minE x y = if (rank x) >= (rank y) then (Bin ((rank y) + 1) minE x y) else (Bin ((rank x) + 1) minE x y)
+makeBin minE x y = if (rank x) >= (rank y) then (Bin ((rank y) + 1) minE x y) else (Bin ((rank x) + 1) minE y x)
 
 findMin (Bin rank minE _ _) = minE
 
